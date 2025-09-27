030630fb56fd14d7a28eb2bd559e83f0748e83fd
Wing <git@public.wing.osaka>
2025-09-27 09:45:01 +0000

    subslot

---
+++ src/content/config.ts
@@ -0,0 +1,18 @@
+// 1. `astro:content`からユーティリティをインポート
+import { z, defineCollection } from 'astro:content';
+
+// 2. コレクションを定義
+const policyCollection = defineCollection({
+  type: 'content', // v2.5.0以降
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    lastUpdated: z.date(),
+  }),
+});
+
+// 3. コレクションを登録するために、単一の`collections`オブジェクトをエクスポート
+//    このキーは、"src/content"のコレクションのディレクトリ名と一致する必要があります。
+export const collections = {
+  'policy': policyCollection,
+};
