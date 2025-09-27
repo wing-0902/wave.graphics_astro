d4cc5b2b02fef5acb265563fb2d72be6b02d7cf9
Wing <git@public.wing.osaka>
2025-09-27 10:10:52 +0000

    svelte

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,15 @@
 // @ts-check
 import { defineConfig, passthroughImageService  } from 'astro/config';
 
+import svelte from '@astrojs/svelte';
+
 // https://astro.build/config
 export default defineConfig({
   output: 'static',
+
   image: {
     service: passthroughImageService(),
   },
+
+  integrations: [svelte()],
 });
