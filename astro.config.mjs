9e7d5c27fa353bc8c61d854787b53db4b6783e83
Wing <git@public.wing.osaka>
2025-09-27 09:08:31 +0000

    いいねえ

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,10 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, passthroughImageService  } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  output: 'static',
+  image: {
+    service: passthroughImageService(),
+  },
+});
