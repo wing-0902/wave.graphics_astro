5f2e0e9374e38378339420b2527dce9d80d13eee
Wing <git@public.wing.osaka>
2025-09-28 06:45:10 +0000

    reactを追加

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import svelte from '@astrojs/svelte';
 
 import mdx from '@astrojs/mdx';
 
+import react from '@astrojs/react';
+
 // https://astro.build/config
 export default defineConfig({
   output: 'static',
@@ -13,5 +15,5 @@ export default defineConfig({
     service: passthroughImageService(),
   },
 
-  integrations: [svelte(), mdx()],
+  integrations: [svelte(), mdx(), react()],
 });
