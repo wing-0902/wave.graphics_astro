b4f37e881b5212ab2a85db3eb66acae93d76f788
Wing <git@public.wing.osaka>
2025-10-03 13:52:25 +0000

    なるほど

--- src/components/SearchUI/SearchSlot.vue
+++ src/components/SearchUI/SearchSlot.vue
@@ -11,5 +11,25 @@
 </script>
 
 <template>
-  <div class="search"></div>
+  <section class='searchBox'>
+    <div class="search"></div>
+  </section>
 </template>
+
+<style lang="scss">
+  .searchBox {
+    .search {
+      --pagefind-ui-scale: 1;
+      --pagefind-ui-primary: #034ad8;
+      --pagefind-ui-text: var(--foreground);
+      --pagefind-ui-background: transparent;
+      --pagefind-ui-border: var(--button-hover-border);
+      --pagefind-ui-tag: var(--foregroundSub);
+      --pagefind-ui-border-width: 2px;
+      --pagefind-ui-border-radius: 8px;
+      --pagefind-ui-image-border-radius: 8px;
+      --pagefind-ui-image-box-ratio: 3 / 2;
+      --pagefind-ui-font: ZenMaru;
+    }
+  }
+</style>
