7bdc92b0ecd008d79e16347595c4983dfac99c2f
Wing <git@public.wing.osaka>
2025-10-04 06:38:26 +0000

    まあまあええな．

---
+++ src/components/LayoutUI/CurrentPath.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  import { onMount } from 'svelte';
+
+  // 初期値は空にしておき、クライアントサイドでパスが設定されるまで待つ
+  let currentPath = '';
+
+  onMount(() => {
+    // コンポーネントがブラウザでマウントされたときに実行される
+    if (typeof window !== 'undefined') {
+      // window.location.pathname はパス名（例: /not-found-page）を返す
+      currentPath = window.location.pathname;
+      currentPath = currentPath.replaceAll("/", " > ");
+    }
+  });
+</script>
+
+<div class="current-path-display">
+  {#if currentPath}
+    <p>https://wave.graphics<strong>{currentPath}</strong></p>
+  {:else}
+    <p>パスを取得中です...</p>
+  {/if}
+</div>
