8a9d4f7fb9c0339515fe0696cf9ed7fc639bd5fb
Wing <git@public.wing.osaka>
2025-10-04 06:05:49 +0000

    ::placeholder

--- src/components/FormUI/FormDisplay.svelte
+++ src/components/FormUI/FormDisplay.svelte
@@ -188,4 +188,22 @@
     outline: none;
     color: var(--a-default)
   }
+
+  /* WebKit */
+  ::-webkit-input-placeholder {
+    color: var(--foregroundSub);
+    opacity: 0.6;
+  }
+
+  /* Firefox */
+  ::-moz-placeholder {
+    color: var(--foregroundSub); /* 任意のカラーコード */
+    opacity: 0.6; /* ★重要：Firefoxでの薄さを解消 */
+  }
+
+  /* 共通 */
+  ::placeholder {
+    color: var(--foregroundSub);
+    opacity: 0.6;
+  }
 </style>
