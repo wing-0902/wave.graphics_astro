a913feefda87d3319a685a5717762d555b89ab19
Wing <git@public.wing.osaka>
2025-10-12 00:14:42 +0000

    backToTop

---
+++ src/components/LayoutUI/BackToTop.vue
@@ -0,0 +1,43 @@
+<template>
+  <button v-show="showButton" @click="returnTop" class="back-to-top">
+    Topへ戻る
+  </button>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        showButton: false,
+        scrollPosition: 0,
+        showAtPixel: 100,
+      };
+    },
+    methods: {
+      returnTop() {
+        window.scrollTo({
+          top: 0,
+          behavior: "smooth",
+        });
+      },
+      handleScroll() {
+        this.scrollPosition = window.scrollY;
+        this.showButton = this.scrollPosition > this.showAtPixel;
+      }
+    },
+    mounted() {
+      window.addEventListener('scroll', this.handleScroll);
+    },
+    beforeDestroy() {
+      window.removeEventListener('scroll', this.handleScroll);
+    }
+  };
+</script>
+
+<style scoped>
+  .back-to-top {
+    position: fixed;
+    bottom: 20px;
+    right: 20px;
+  }
+</style>
