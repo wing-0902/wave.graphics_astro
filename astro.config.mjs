022e82af49eb707f401e0936b32c1b9ef41d80e3
Wing <git@public.wing.osaka>
2025-10-21 13:06:48 +0000

    OK

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,14 +4,12 @@ import { defineConfig, passthroughImageService  } from 'astro/config';
 import remarkMath from 'remark-math';
 import rehypeKatex from 'rehype-katex';
 
+import rehypeExternalLinks from 'rehype-external-links';
+
 import svelte from '@astrojs/svelte';
-
 import mdx from '@astrojs/mdx';
-
 import react from '@astrojs/react';
-
 import sitemap from '@astrojs/sitemap';
-
 import vue from '@astrojs/vue';
 
 // https://astro.build/config
@@ -30,7 +28,17 @@ export default defineConfig({
 
   markdown: {
     remarkPlugins: [remarkMath],
-    rehypePlugins: [rehypeKatex],
+    rehypePlugins: [
+      rehypeKatex,
+      [
+        rehypeExternalLinks,
+        {
+          target: '_blank',
+          rel: ['noopener', 'noreferrer'],
+          site: 'https://wave.graphics'
+        }
+      ],
+    ],
   },
 
   integrations: [svelte(), mdx(), react(), sitemap(), vue()],
--- src/content/framework/astro.md
+++ src/content/framework/astro.md
@@ -7,3 +7,11 @@ githubUrl: https://github.com/withastro
 icon: https://astro.build/favicon.svg
 themeColor: rgba(255, 34, 163, 1)
 ---
+
+### Astroについて
+このフレームワークは，当サイトを作成する上で，土台になっているものです．<br />
+Astroは，いわゆるSSG（静的サイトジェネレータ）であり，予め表示される内容が決まっているサイトの制作にぴったりです．<br />
+さらに，Astroは事前にブラウザが直接理解できる形式にデータを変換するため，非常に高速に閲覧できるというメリットもあります．<br />
+
+### Why Astro?
+Astroには，[コンテンツコレクション](https://docs.astro.build/ja/guides/content-collections/)という概念があります．
