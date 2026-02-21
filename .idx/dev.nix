cd3c1d6e473c40f898e63e22fab28b68a8b12ef8
Wing <git@public.wing.osaka>
2026-02-21 07:18:38 +0000

    [skip ci] renew nixos config

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -2,7 +2,7 @@
 # see: https://firebase.google.com/docs/studio/customize-workspace
 { pkgs, ... }: {
   # Which nixpkgs channel to use.
-  channel = "stable-25.11"; # or "unstable"
+  channel = "stable-25.05"; # or "unstable"
 
   # Use https://search.nixos.org/packages to find packages
   packages = [
