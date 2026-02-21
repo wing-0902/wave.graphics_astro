4f9cacaaad64c72c0cf97af1ada19d6e451917ab
Wing <git@public.wing.osaka>
2026-02-21 16:14:37 +0900

    Add Nix configuration for development environment [skip ci]
    for Firebase Studio

---
+++ .idx/dev.nix
@@ -0,0 +1,51 @@
+# To learn more about how to use Nix to configure your environment
+# see: https://firebase.google.com/docs/studio/customize-workspace
+{ pkgs, ... }: {
+  # Which nixpkgs channel to use.
+  channel = "stable-25.11"; # or "unstable"
+
+  # Use https://search.nixos.org/packages to find packages
+  packages = [
+    pkgs.nodejs_24
+    pkgs.pnpm
+  ];
+
+  # Sets environment variables in the workspace
+  env = {};
+  idx = {
+    # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
+    extensions = [
+      "ms-vscode.vscode-typescript-next"
+      "astro-build.astro-vscode"
+      "sdras.night-owl"
+      "svelte.svelte-vscode"
+      "Vue.volar"
+    ];
+
+    # Enable previews
+    previews = {
+      enable = true;
+      previews = {
+        web = {
+          command = ["pnpm" "run" "dev" "--host" "0.0.0.0" "--port" "$PORT"];
+          manager = "web";
+        };
+      };
+    };
+
+    # Workspace lifecycle hooks
+    workspace = {
+      # Runs when a workspace is first created
+      onCreate = {
+        # Example: install JS dependencies from NPM
+        # npm-install = "npm install";
+        pnpm-install = "pnpm install";
+      };
+      # Runs when the workspace is (re)started
+      onStart = {
+        # Example: start a background task to watch and re-build backend code
+        # watch-backend = "npm run watch-backend";
+      };
+    };
+  };
+}
